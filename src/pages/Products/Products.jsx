63ff47292834c82ec539ea128d9d86d87132c9b8
Okuku Itorobong <patrickokuku1@gmail.com>
2023-06-24 22:12:28 +0100

    responsive: categories page

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import List from "../../components/List/List";
 import { useParams } from "react-router-dom";
 import "./Products.scss";
@@ -10,6 +10,20 @@ const Products = () => {
   const [maxPrice, setMaxPrice] = useState(1000);
   const [sort, setSort] = useState("asc");
   const [selectedSubCats, setSelectedSubCats] = useState([]);
+  const [viewportWidth, setViewportWidth] = useState(0);
+
+  function handleResize() {
+    setViewportWidth(window.innerWidth);
+  }
+
+  useEffect(() => {
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   const { data, loading, error } = useFetch(
     `sub-categories/?[filters][categories][id][$eq]=${catId}`
@@ -28,62 +42,83 @@ const Products = () => {
 
   return (
     <div className="products">
-      <div className="left">
-        <div className="filterItem">
-          <h2>Product Categories</h2>
-          {data?.map((item) => (
-            <div className="inputItem" key={item.id}>
+      {viewportWidth > 990 && (
+        <div className="left">
+          <div className="filterItem">
+            <h2>Product Categories</h2>
+            {data?.map((item) => (
+              <div className="inputItem" key={item.id}>
+                <input
+                  type="checkbox"
+                  name=""
+                  id={item.id}
+                  value={item.id}
+                  onChange={handleChange}
+                />
+                <label htmlFor={item.id}>{item.attributes.title}</label>
+              </div>
+            ))}
+          </div>
+          <div className="filterItem">
+            <h2>Filter by price</h2>
+            <div className="inputItem">
+              <span>0</span>
               <input
-                type="checkbox"
-                name=""
-                id={item.id}
-                value={item.id}
-                onChange={handleChange}
+                type="range"
+                min={0}
+                max={1000}
+                onChange={(e) => setMaxPrice(e.target.value)}
               />
-              <label htmlFor={item.id}>{item.attributes.title}</label>
+              <span>{maxPrice}</span>
             </div>
-          ))}
-        </div>
-        <div className="filterItem">
-          <h2>Filter by price</h2>
-          <div className="inputItem">
-            <span>0</span>
-            <input
-              type="range"
-              min={0}
-              max={1000}
-              onChange={(e) => setMaxPrice(e.target.value)}
-            />
-            <span>{maxPrice}</span>
+          </div>
+          <div className="filterItem">
+            <h2>Sort by</h2>
+            <div className="inputItem">
+              <input
+                type="radio"
+                id="asc"
+                value="asc"
+                name="price"
+                checked
+                onChange={(e) => setSort("asc")}
+              />
+              <label htmlFor="asc">Price (Lowest first)</label>
+            </div>
+            <div className="inputItem">
+              <input
+                type="radio"
+                id="desc"
+                value="desc"
+                name="price"
+                onChange={(e) => setSort("desc")}
+              />
+              <label htmlFor="desc">Price (Highest first)</label>
+            </div>
           </div>
         </div>
-        <div className="filterItem">
-          <h2>Sort by</h2>
-          <div className="inputItem">
-            <input
-              type="radio"
-              id="asc"
-              value="asc"
-              name="price"
-              checked
-              onChange={(e) => setSort("asc")}
-            />
-            <label htmlFor="asc">Price (Lowest first)</label>
-          </div>
-          <div className="inputItem">
-            <input
-              type="radio"
-              id="desc"
-              value="desc"
-              name="price"
-              onChange={(e) => setSort("desc")}
-            />
-            <label htmlFor="desc">Price (Highest first)</label>
-          </div>
-        </div>
-      </div>
+      )}
       <div className="right">
         <CategoryHome id={catId} />
+        {viewportWidth < 990 && (
+          <>
+            <h2>Product Categories</h2>
+            <div className="filters">
+              {data?.map((item) => (
+                <div className="item-category" key={item.id}>
+                  <input
+                    type="checkbox"
+                    name=""
+                    id={item.id}
+                    value={item.id}
+                    onChange={handleChange}
+                  />
+                  <label htmlFor={item.id}>{item.attributes.title}</label>
+                </div>
+              ))}
+            </div>
+          </>
+        )}
         <List
           catId={catId}
           maxPrice={maxPrice}
